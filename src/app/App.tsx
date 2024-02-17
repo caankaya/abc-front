c09b0fb850c4601b0187e301f932fca6bb00331d
Canoral <kaya68@outlook.com>
2024-02-17 18:13:32 +0100

    Mise en place du paragraphe de la page d'accueil et le bouton pour créer un nouveau scénario

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,17 @@
 export default function App() {
-  return <div className="app">App</div>;
+  return (
+    <main className="app">
+      <div className="flex flex-col gap-y-5 shadow absolute p-5 rounded-lg left-1/2 top-1/3 transform -translate-x-1/2 -translate-y-1/2">
+        <p className="text-sm text-justify">
+          Vous devez préparer un nouveau cours et vous aimeriez innover en
+          intégrant des activités d’apprentissage, mais vous ne savez pas quoi
+          et comment faire? Ou alors vous êtes responsable d’une formation, d’un
+          module dont vous aimeriez repenser l’organisation avec les
+          enseignants, mais vous ne savez pas comment vous y prendre ? Alors
+          l’application en ligne « ABC Learning » peuvent vous aider.
+        </p>
+        <button className="btn btn-sm m-auto">Créer un nouveau scénario</button>
+      </div>
+    </main>
+  );
 }

af58627227c8e89386a14a5528ba1dffe0fbabf5
Canoral <kaya68@outlook.com>
2024-02-17 09:14:50 +0100

    build: Mise en place Daisy UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,15 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+
+  // daisyUI config (optional - here are the default values)
+  daisyui: {
+    themes: ["nord"],
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+  },
 };

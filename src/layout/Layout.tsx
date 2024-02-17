b0491e2320b0c6f29234b26a040eb01c23c69981
Canoral <kaya68@outlook.com>
2024-02-17 08:59:37 +0100

    feat: Mise en place react router dom

---
+++ src/layout/Layout.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
+import Header from "./Header";
+
+export default function Layout() {
+  return (
+    <div className="layout">
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from "./layout/Layout";
+import App from "./app/App";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />} errorElement={""}>
+      <Route errorElement={""}>
+        <Route index element={<App />} />
+        {/* Compléter le router avec d'autres pages à partir de là*/}
+        {/* Oubliez pas d'ajouter Outlet dans le composant Layout*/}
+      </Route>
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />
+);

52e0d218209f223e80fd8790893c7785eee16b2b
Canoral <kaya68@outlook.com>
2024-02-17 14:08:41 +0100

    feat: Responsive Design de Header

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -2,19 +2,38 @@ import { faBookOpen, faGraduationCap } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import { togglerDropdown } from "../../redux/reducers/header";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function Header() {
   const dispatch = useAppDispatch();
   const isOpen = useAppSelector((state) => state.header.buttonBurger);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <header className="header">
       <div className="navbar bg-base-100">
         <div className="flex-1">
-          <a className="text-xl font-bold">LOGO</a>
-          <a className="text-sm ml-5">Création de scénario d'apprentissage</a>
+          <div className="w-26">
+            <Link to={"/"}>
+              <img src="/src/assets/logo.png" alt="" width={100} />
+            </Link>
+          </div>
+          <p className="text-sm ml-5 text-center">Création de scénario</p>
         </div>
-        <ul className="menu menu-horizontal px-1">
+        <ul
+          className={`menu menu-horizontal px-1 ${
+            windowWidth < 768 ? "hidden" : "visible"
+          }`}
+        >
           <li>
             <a href="https://moodletoolguide.net/fr/" target="_blank">
               Guide des outils Moodle <FontAwesomeIcon icon={faGraduationCap} />
@@ -53,12 +72,37 @@ export default function Header() {
           ></span>
         </button>
         <div
-          tabIndex={0}
-          className={`p-2 bg-base-100 rounded-box shadow w-52 absolute right-0 mr-5 mt-28 ${
+          className={`p-2 bg-base-100 rounded-box shadow w-52 absolute top-16 right-5 ${
             isOpen ? "visible" : "hidden"
           }`}
         >
-          <a className="btn btn-sm btn-ghost btn-block">Se connecter</a>
+          <ul>
+            <li>
+              <a className="btn btn-sm btn-ghost btn-block">Se connecter</a>
+            </li>
+            {windowWidth < 768 && (
+              <>
+                <li>
+                  <a
+                    className="btn btn-sm btn-ghost btn-block"
+                    href="https://moodletoolguide.net/fr/"
+                    target="_blank"
+                  >
+                    Guide des outils Moodle
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="btn btn-sm btn-ghost btn-block"
+                    href="https://h5p.org/content-types-and-applications"
+                    target="_blank"
+                  >
+                    Exemples H5P
+                  </a>
+                </li>
+              </>
+            )}
+          </ul>
         </div>
       </div>
     </header>
